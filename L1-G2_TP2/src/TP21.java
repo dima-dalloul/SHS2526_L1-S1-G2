cfaf8ee4295044e4d09d3139e6adb5e9de757d25
Dima Dalloul <dima.dalloul@univ-grenoble-alpes.fr>
2025-09-22 16:05:15 +0200

    Correction exercice 1 du TP2

--- L1-G2_TP2/src/TP21.java
+++ L1-G2_TP2/src/TP21.java
@@ -1,29 +1,4 @@
 import java.util.Scanner;
-
-/*
-
-Le 23/9 est un vendredi
-
-
-Pour compter le nombre de jours depuis le 1er janvier, vous pouvez additionner le numéro du jour avec le nombre
-de jours des mois pleins qui précèdent (31 pour février, 31+28=59 pour mars, 31+28+31=90 pour avril, 120 pour mai,
-151 pour juin, 181 pour juillet, 212 pour août, 243 pour septembre, 273 pour octobre, 304 pour novembre et 334
-pour décembre).
-Ainsi le 26 septembre est le 243+26 = 269e jour de l'année 2025. Comme le 1er janvier est le 1er jour de l'année,
-il y a donc 269-1=268 jours d'écart entre les deux. Le reste de la division de 268 par 7 est 2. Donc le jour de
-la semaine correspondant au 26 septembre est décalé de 2 jours par rapport à celui du 1er janvier. C'est donc un
-vendredi.
-Il est donc conseillé d'écrire votre programme de la manière suivante :
-1.
-lire depuis le clavier le numéro du jour et le numéro du mois ;
-2.
-calculer le nombre de jours depuis le 1er janvier avec une série de tests (si le mois est 1 alors... sinon si le
-mois est 2 alors...) ;
-3.
-calculer le décalage avec le 1er janvier du jour de la semaine ;
-4.
-transformer ce nombre en un jour de la semaine avec une
- */
 public class TP21 {
     public static void main (String[] args){
         int     jour = 0,
